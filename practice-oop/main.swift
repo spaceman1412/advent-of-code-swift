5f115e61db30d2737b86b22460188d624457e4fd
spaceman1412 <spacemannormal@gmail.com>
2024-10-25 11:40:42 +0700

    finish day 1 part 1

--- practice-oop/main.swift
+++ practice-oop/main.swift
@@ -7,5 +7,45 @@
 
 import Foundation
 
-print("Hello, World!")
+let filePath = "/Users/chaileasevn/Downloads/input.txt"
+
+func part1() -> Int? {
+    do {
+        let contents = try String(contentsOfFile: filePath, encoding: .utf8)
+        let arrayOfText = contents.split(separator: "\n")
+        var sum = 0
+        
+        for text in arrayOfText {
+            var charNumArr: [Int] = [0,0]
+            var firstCharPosition = text.count
+            var lastCharPosition = -1
+            
+            for (offset,char) in text.enumerated()  {
+                    if let num = char.wholeNumberValue {
+                        if firstCharPosition > offset {
+                            firstCharPosition = offset
+                            charNumArr[0] = num
+                        }
+                        
+                        if lastCharPosition < offset {
+                            lastCharPosition = offset
+                            charNumArr[1] = num
+                        }
+                    }
+                if offset == text.count - 1 && charNumArr.count == 2 {
+                    let textNumSum = (charNumArr[0]*10)+charNumArr[1]
+                    sum = sum + textNumSum
+                    break
+                }
+            }
+        }
+        
+        return (sum)
+    }
+    catch let error as NSError {
+        print("Ooops! Something went wrong: \(error)")
+    }
+    return nil
+}
+
 
